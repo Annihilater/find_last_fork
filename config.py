b1b618b9816fc700821b11f2302d5b97b5fa2086
Annihilater <yanmiexingkong@gmail.com>
2019-11-22 18:12:38 +0800

    feat: add config:proxy config

---
+++ config.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date  : 2019/11/22 17:16
+# @Author: yanmiexingkong
+# @email : yanmiexingkong@gmail.com
+# @File  : config.py
+
+# 本地开的socket5代理地址是 127.0.0.1:8889
+PROXIES = {
+    'http': 'socks5://127.0.0.1:8889',
+    'https': 'socks5://127.0.0.1:8889'
+}
